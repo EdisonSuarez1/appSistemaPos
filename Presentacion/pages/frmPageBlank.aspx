79963f68cb0fe92c3e279aff41751851ed2848e4
EdisonSuarez1 <easuarez94@misena.edu.co>
2022-05-03 20:05:19 -0500

    Avances esteticos de plantilla

---
+++ Presentacion/pages/frmPageBlank.aspx
@@ -0,0 +1,1 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Presentacion/Site1.Master" AutoEventWireup="true" CodeBehind="frmPageBlank.aspx.cs" Inherits="appPlantilla.Presentacion.pages.frmPageBlank" %>
--- Presentacion/pages/frmPageBlank.aspx.cs
+++ Presentacion/pages/frmPageBlank.aspx.cs
@@ -5,9 +5,9 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-namespace appPlantilla.Presentacion
+namespace appPlantilla.Presentacion.pages
 {
-    public partial class frmBlankPage : System.Web.UI.Page
+    public partial class frmPageBlank : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
